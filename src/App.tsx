24a45e41706e9b41118df5ab0ca5802f84f5bbc7
Victor Souza <victorsouza0106@gmail.com>
2022-09-28 00:34:23 -0300

    -> Adding Basic Files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import EmployeeSelect from "./pages/EmployeeSelect/EmployeeSelect";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route element={MainPage()} path="/" />
+        <Route element={EmployeeSelect()} path="/select" />
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
