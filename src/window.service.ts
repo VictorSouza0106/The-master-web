30ce5959ffe0cdcf8d8db8d613c1e8b204c7bdff
Victor Souza <victorsouza0106@gmail.com>
2022-10-04 20:17:23 -0300

    feat: Window Service

--- src/window.service.ts
+++ src/window.service.ts
@@ -1,1 +1,28 @@
-export const WindowService = {}
+const BREAKING_POINTS = {
+  mobile: 780,
+  smallScreen: 1400,
+  mediumScreen: 1900,
+};
+
+export type WindowSize = "mobile" | "small" | "medium" | "large";
+
+export interface IResponsiveAttr {
+  mobile: any;
+  small: any;
+  medium: any;
+  large: any;
+}
+
+export function getCurrentSize(): WindowSize {
+  const windowSize = window.innerWidth;
+  const { mobile, smallScreen, mediumScreen } = BREAKING_POINTS;
+
+  if (windowSize <= mobile) return "mobile";
+  else if (windowSize <= smallScreen) return "small";
+  else if (windowSize <= mediumScreen) return "medium";
+  else return "large";
+}
+
+export function getResponsiveAttr(responsiveObj: IResponsiveAttr) {
+  return responsiveObj[getCurrentSize()];
+}
